1771cf1afd3360b1c73cb20f767a9a2ad6929748
rjclaasen <raphael.claasen@gmail.com>
2023-07-04 23:47:53 +0200

    Implement debounced query input

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import { useMemo, useState } from 'react';
+import debounce from 'lodash.debounce';
 import RepositoriesList from './RepositoriesList'
 
 import './App.css';
 
 function App() {
+  const [query, setQuery] = useState('');
+
+  const changeHandler = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const debouncedChangeHandler = useMemo(
+    () => debounce(changeHandler, 300)
+  , []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h2>My first Apollo app 🚀</h2>
         <br/>
-        <RepositoriesList query="hello world" />
+        <h5>Search!</h5>
+        <input placeholder={"Zoeken doe je hier"} onChange={debouncedChangeHandler} />
+        <RepositoriesList query={query} />
       </header>
     </div>
   );
