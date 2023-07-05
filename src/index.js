a85202ffce55203077f56cb6f986c414d2bb327f
rjclaasen <raphael.claasen@gmail.com>
2023-07-05 22:14:25 +0200

    Add MUI libraries

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ApolloClient, createHttpLink, InMemoryCache, ApolloProvider, gql } from '@apollo/client'
+import { ApolloClient, createHttpLink, InMemoryCache, ApolloProvider } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
