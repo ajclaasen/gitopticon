451f0c58201738efd943db9249d1f1c9d74015f0
rjclaasen <raphael.claasen@gmail.com>
2023-07-05 22:25:11 +0200

    Use loading animations instead of text

--- src/RepositoriesList.js
+++ src/RepositoriesList.js
@@ -2,6 +2,7 @@ import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from './queries';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import RepositoryCard from './RepositoryCard';
+import { CircularProgress } from '@mui/material';
 
 export default function RepositoriesList({ query }) {
   const { loading, error, fetchMore, data } = useQuery(GET_REPOSITORIES, {
@@ -17,14 +18,14 @@ export default function RepositoriesList({ query }) {
     });
   };
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return <CircularProgress />;
   if (error) return <p>Error : {error.message}</p>;
 
   return <InfiniteScroll
     dataLength={data.search.nodes.length}
     next={fetchMoreData}
     hasMore={data.search.pageInfo.hasNextPage}
-    loader={<p>Loading more...</p>}
+    loader={<CircularProgress />}
     endMessage={<p>End of results</p>}
   >
     {data.search.nodes.map((node) => (
